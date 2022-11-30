6631619eadbce2fa19672247638a1778d635a0b2
MasWap <lilian.layrac@gmail.com>
2022-11-30 15:14:53 +0100

    Adding some functions

--- src/app.js
+++ src/app.js
@@ -16,6 +16,42 @@ class Cube {
     }
 }
 
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    getType () {
+
+        if (this.a === this.b && this.a === this.c && this.b === this.c) {
+            return("Equilateral");
+        } else if (this.a === this.b || this.a === this.c || this.b === this.c) { 
+            return("Isosceles");
+        } else if (
+            this.a*this.a === ((this.b+this.c)*(this.b+this.c)),
+            this.b*this.b === ((this.a+this.c)*(this.a+this.c)),
+            this.c*this.c === ((this.a+this.b)*(this.a+this.b))
+            )
+        return("Rectangle");
+    } // Ipothénuse au carré = somme des 2 autres coté au carré
+
+    getAir () {
+        p = (this.a + this.b + this.c)
+        s = p/2
+        air = s(s-this.a)(s-this.b)(s-this.c)
+        return air
+    }
+
+
+    //AB + BC + CA = 3 + 4 + 6 = 13 cm
+    getPerimetre () {
+        return (this.a + this.b + this.c);
+    }
+}
+
 module.exports = {
-    Cube:Cube
+    Cube:Cube,
+    Triangle:Triangle
 }
--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,5 @@
 const Cube = require('../src/app').Cube;
+const Triangle = require('../src/app').Triangle;
 const expect = require('chai').expect;
 
 describe('Testing the Cube Functions', function() {
@@ -21,3 +22,28 @@ describe('Testing the Cube Functions', function() {
     });
     
 });
+
+describe('Testing the Triangle Functions', function() {
+    it('1. The type of the Triangle', function(done) {
+        let t1 = new Triangle(5, 5, 5);
+        let t2 = new Triangle(4, 4, 5);
+        let t3 = new Triangle(9.6, 2.8, 10);
+        expect(t1.getType()).to.equal("Equilateral");
+        expect(t2.getType()).to.equal("Isosceles");
+        expect(t3.getType()).to.equal("Rectangle");
+        done();
+    });
+
+    it('2. The air of the Triangle', function(done) {
+        let t4 = new Triangle(3, 4, 5);
+        expect(t4.getAir()).to.equal("");
+        done();
+    });
+
+    it('2. The perimetre of the Triangle', function(done) {
+        let t5 = new Triangle(3, 4, 5);
+        expect(t5.getPerimetre()).to.equal(12);
+        done();
+    });
+
+})
